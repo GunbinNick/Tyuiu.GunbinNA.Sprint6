55f27e0a0428d31f33061f3235581ee4a89cd5c6
Tyuiu.GunbinNA@outlook.com <gunbin1005@gmail.com>
2023-12-10 19:36:28 +0500

    CorrectErrors

---
+++ Tyuiu.GunbinNA.Sprint6.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.GunbinNA.Sprint6.Task7.V1.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.GunbinNA.Sprint6.Task7.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GunbinNA.Sprint6.Task7.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GunbinNA.Sprint6.Task7.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8c77dcab-3441-4b81-8246-e3e8f95458e7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
